8dac955a899609b48255a4ae3517270a10f26104
Jacob Kravits <kravitsjacob@gmail.com>
2022-05-30 10:44:01 -0600

    General template for submission.py

---
+++ submission.py
@@ -0,0 +1,17 @@
+"""Player submission script"""
+
+
+def reward():
+    """
+    Reward function
+    """
+    print('This is the reward function')
+    return 0
+
+
+def predict():
+    """
+    Predict function
+    """
+    print('This is the predict function')
+    return 0
