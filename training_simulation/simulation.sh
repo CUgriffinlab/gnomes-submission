824ec7962a9449027f9f37590f2e5854b71e2424
kravitsjacob <kravitsjacob@gmail.com>
2022-06-21 16:05:33 -0600

    Outline of instructions script

--- training_simulation/simulation.sh
+++ training_simulation/simulation.sh
@@ -1,11 +1,14 @@
 # Pull redis docker
 docker pull redis
 
-# Start redis docker
-docker run --name redis-docker -d redis
-
 # Build simulation docker
 docker build --tag simulation .
 
+# Start redis
+docker run --name dragg-redis -d redis
+
 # Run simulation docker
-docker run simulation
+# docker run --name simulation --link dragg-redis:redis -d simulation
+# docker run --name simulation --link dragg-redis:simulation --rm redis redis-cli -h redis -p 6379
+# docker run -it --name simulation --link dragg-redis:simulation simulation redis redis-cli -h redis -p 6379
+# docker run -t --name simulation --link dragg-redis:simulation -h redis simulation
