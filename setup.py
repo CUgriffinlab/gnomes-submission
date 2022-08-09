e54a420c51cbdc22ac3899aace13dcdf8d66b459
kravitsjacob <kravitsjacob@gmail.com>
2022-08-09 15:12:15 -0600

    Remove author information

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="submission",
     version="0.0.0b0",
-    author="Jacob Kravits",
-    author_email="kravitsjacob@gmail.com",
-    description="Submission for NSF CAREER commpetition",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,5 +10,5 @@ setuptools.setup(
     ],
     package_dir={"": "."},
     packages=setuptools.find_packages(where="."),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
