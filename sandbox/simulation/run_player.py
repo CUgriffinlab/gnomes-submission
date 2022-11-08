2a00bc5f6da70f5d0d8a040a17bf52ec9b00a4b6
Aisling Pigott <aisling.pigott@colorado.edu>
2022-11-08 14:53:04 -0700

    updated to run with an RL agent for the player submission, added reset feature to control training period vs testing period

--- sandbox/simulation/run_player.py
+++ sandbox/simulation/run_player.py
@@ -3,18 +3,32 @@ import asyncio
 import argparse
 
 from dragg_comp.player import PlayerHome
-from submission import predict, reward
+# from submission import predict, reward
+# from rl_submission import reward, create_model
+import rl_submission as submission
+from stable_baselines3 import SAC
 
 REDIS_URL = "redis://localhost"
 
 class PlayerSubmission(PlayerHome):
-	def __init__(self, redis_url=REDIS_URL):
+	def __init__(self, redis_url=REDIS_URL, rl_name="my_agent"):
 		super().__init__(redis_url=redis_url)
+		
+		self.rl_name = rl_name
+		self.agent = None
 
 	def get_reward(self):
 		# redefines get_reward with the player's implementation
-		my_reward = reward(self)
+		my_reward = submission.reward(self)
 		return my_reward
+
+	def get_prediction(self):
+		if self.rl_name:
+			if not self.agent:
+				self.agent = model.load(f"../submission/{self.rl_name}")
+			return self.agent.predict(self.get_obs())[0]
+		else:
+			return submission.predict(self)
 
 if __name__=="__main__":
 
@@ -22,14 +36,24 @@ if __name__=="__main__":
 
 	#-db DATABSE -u USERNAME -p PASSWORD -size 20
 	parser.add_argument("-r", "--redis", help="Redis host URL", default=REDIS_URL)
-
 	args = parser.parse_args()
 
+	env = PlayerSubmission(redis_url=args.redis, rl_name=submission.RL_NAME)
+	obs = env.reset()
+	
+	rl = True
+	if rl:
+		model = submission.create_model(env)
+		model.learn(submission.NUM_TIMESTEPS)
+		model.save(f"../submission/{env.rl_name}")
+
+	obs = env.reset()
 	tic = datetime.now()
-	env = PlayerSubmission(redis_url=args.redis)
-	env.reset()
-	for _ in range(env.num_timesteps * env.home.dt):
-		action = predict(env)
+	for _ in range(env.num_timesteps):
+		if rl:
+			action = env.get_prediction()
+		else:
+			action = predict(env)
 		env.step(action)
 
 	asyncio.run(env.post_status("done"))
