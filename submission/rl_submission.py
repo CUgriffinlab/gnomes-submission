197bac79f115520f20d1ce08ad2ac81ffcf60d9f
Aisling Pigott <aisling.pigott@colorado.edu>
2022-11-11 11:47:31 -0700

    add user input for new states

--- submission/rl_submission.py
+++ submission/rl_submission.py
@@ -2,6 +2,24 @@ from stable_baselines3 import SAC
 
 RL_NAME = "my_agent"
 NUM_TIMESTEPS = 25
+OBSERVATIONS = {
+	"leaving_horizon":	True,
+	"returning_horizon":True,
+	"occupancy_status": True,
+	"future_waterdraws":True,
+	"t_out":			True,
+	"t_out_6hr":		True,
+	"t_out_12hr":		True,
+	"ghi":				True,
+	"ghi_6hr":			True,
+	"ghi_12hr":			True,
+	"t_in":				True,
+	"t_wh":				True,
+	"e_ev":				True,
+	"time_of_day":		True,
+	"community_demand":	True,
+	"my_demand": 		True
+}
 
 def reward(home):
     """
