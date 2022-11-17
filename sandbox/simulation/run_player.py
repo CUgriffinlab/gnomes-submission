9e992b97a21b370df1a61ac7d60cdad13207b04a
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-11-17 14:03:00 -0700

    Revert run_player to rbc

--- sandbox/simulation/run_player.py
+++ sandbox/simulation/run_player.py
@@ -3,33 +3,19 @@ import asyncio
 import argparse
 
 from dragg_comp.player import PlayerHome
-# import submission.rbc as submission
-import submission.rl as submission
-from stable_baselines3 import SAC
+from submission import predict, reward
+
 
 REDIS_URL = "redis://localhost"
 
 class PlayerSubmission(PlayerHome):
-	def __init__(self, redis_url=REDIS_URL, rl_name="my_agent"):
+	def __init__(self, redis_url=REDIS_URL):
 		super().__init__(redis_url=redis_url)
-		
-		self.rl_name = rl_name
-		if self.rl_name:
-			self.update_states(submission.OBSERVATIONS)
-		self.agent = None
 
 	def get_reward(self):
 		# redefines get_reward with the player's implementation
-		my_reward = submission.reward(self)
+		my_reward = reward(self)
 		return my_reward
-
-	def get_prediction(self):
-		if self.rl_name:
-			if not self.agent:
-				self.agent = SAC.load(f"../../submission/{self.rl_name}")
-			return self.agent.predict(self.get_obs())[0]
-		else:
-			return submission.predict(self)
 
 if __name__=="__main__":
 
@@ -37,29 +23,17 @@ if __name__=="__main__":
 
 	#-db DATABSE -u USERNAME -p PASSWORD -size 20
 	parser.add_argument("-r", "--redis", help="Redis host URL", default=REDIS_URL)
+
 	args = parser.parse_args()
 
-	env = PlayerSubmission(redis_url=args.redis, rl_name=submission.RL_NAME)
-	
-	rl = True
-	if rl:
-		print("Training your RL agent...")
-		model = submission.create_model(env)
-		model.learn(submission.NUM_TIMESTEPS)
-		model.save(f"../../submission/{env.rl_name}")
-
-	obs = env.reset()
 	tic = datetime.now()
-	print("Testing your agent...")
-	for _ in range(env.num_timesteps):
-		if rl:
-			action = env.get_prediction()
-		else:
-			action = predict(env)
+	env = PlayerSubmission(redis_url=args.redis)
+	env.reset()
+	for _ in range(env.num_timesteps * env.home.dt):
+		action = predict(env)
 		env.step(action)
 
 	asyncio.run(env.post_status("done"))
 	print(env.score())
 	toc = datetime.now()
 	print(toc-tic)
-
