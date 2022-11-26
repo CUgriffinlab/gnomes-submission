8fc2952b2ecc1fbb47e25f2114442ce5a7f657d1
Aisling Pigott <aisling.pigott@colorado.edu>
2022-11-25 20:07:07 -0700

    adapted to rbc rl agnostic

--- sandbox/simulation/run_player.py
+++ sandbox/simulation/run_player.py
@@ -1,31 +1,14 @@
 RL_TRAINING = False
 RL_NAME = None
 
-
 from datetime import datetime
 import asyncio
 import argparse
 
 from dragg_comp.player import PlayerHome
-from submission.submission import *
+from submission import *
 
 REDIS_URL = "redis://localhost"
-
-class PlayerSubmission(PlayerHome):
-	def __init__(self, redis_url=REDIS_URL):
-		super().__init__(redis_url=redis_url)
-		self.rl_name = rl_name
-		if self.rl_name:
-			self.update_states(OBSERVATIONS)
-		self.agent = None
-
-	def get_reward(self):
-		# redefines get_reward with the player's implementation
-		my_reward = reward(self)
-		return my_reward
-
-	def get_prediction(self):
-		return predict(self)
 
 if __name__=="__main__":
 
@@ -36,19 +19,13 @@ if __name__=="__main__":
 
 	args = parser.parse_args()
 
-	env = PlayerSubmission(redis_url=args.redis, rl_name=RL_NAME)
-	
-	if RL_TRAINING:
-		print("Training your RL agent...")
-		model = create_model(env)
-		model.learn(NUM_TIMESTEPS)
-		model.save(f"../../submission/{env.rl_name}")
+	env = TestingEnv(redis_url=args.redis)
 
 	obs = env.reset()
 	tic = datetime.now()
 	print("Testing your agent...")
 	for _ in range(env.num_timesteps):
-		action = env.get_prediction()
+		action = submission.predict(env)
 		env.step(action)
 
 	asyncio.run(env.post_status("done"))
