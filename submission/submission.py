a069e3287450a51a3112bf12314f0aa67578b15f
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-12-01 15:31:10 -0700

    Add more information to submission.py

--- submission/submission.py
+++ submission/submission.py
@@ -1,16 +1,31 @@
+"""Template for submission"""
+
+
 def predict(home):
     """
-    Predict function
-    :input: the current state of the environment as a list
-    :output:
+    Simple rule-based prediction function as a template
+
+    Parameters
+    ----------
+    home : dragg_comp.player.PlayerHome
+        Your home
+
+    Returns
+    -------
+    list
+        List of actions corresponding to hvac, wh, and electric vehicle
     """
-    if home.obs_dict['occupancy_status'] == 0:
-        hvac_action = 0 # neutral, leave the hvac alone
-        wh_action = -1 # turn off the water heater
-        ev_action = 1 # charge the car
+    # Rule-based control
+    if home.obs_dict['occupancy_status'] == 0:  # no one home
+        hvac_action = 0  # neutral, leave the hvac alone
+        wh_action = -1  # turn off the water heater
+        ev_action = 1  # charge the car
         action = [hvac_action, wh_action, ev_action]
 
-    else: 
-        action = home.action_space.sample() # choose a random action
+    else:  # Someone is home
+        action = home.action_space.sample()  # choose a random action
+
+    # Ensure all actions are positive
+    action = [0 if i < 0 else i for i in action]
 
     return action
