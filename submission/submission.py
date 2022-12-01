1bd2e1ca32ffb5e2bc855fe6673491d47f9d47d1
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2022-12-01 14:56:03 -0700

    Remove RL submission (moved to other repo)

--- submission/submission.py
+++ submission/submission.py
@@ -1,13 +1,9 @@
-# for an RBC agent
-
 def predict(home):
     """
     Predict function
     :input: the current state of the environment as a list
     :output:
     """
-    threshold = 2 # kW
-
     if home.obs_dict['occupancy_status'] == 0:
         hvac_action = 0 # neutral, leave the hvac alone
         wh_action = -1 # turn off the water heater
@@ -17,13 +13,4 @@ def predict(home):
     else: 
         action = home.action_space.sample() # choose a random action
 
-    return action
-
-# for an RL agent
-from submission.rl_training import *
-
-def predict(home):
-    agent = SAC.load("../../submission/my_test")
-    norm_obs = normalization(home)
-    action = agent.predict(norm_obs)[0]
     return action
