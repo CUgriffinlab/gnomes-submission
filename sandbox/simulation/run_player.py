f3a75ba4fe032d8f6b0a208bf6151cc705669a37
Jacob Kravits <49840473+kravitsjacob@users.noreply.github.com>
2023-01-03 16:04:30 -0700

    Add try to sandbox

--- sandbox/simulation/run_player.py
+++ sandbox/simulation/run_player.py
@@ -4,31 +4,42 @@ RL_NAME = None
 from datetime import datetime
 import asyncio
 import argparse
+import pandas as pd
 
 from dragg_comp.player import PlayerHome
-from submission.submission import *
 
 REDIS_URL = "redis://localhost"
 
 if __name__=="__main__":
 
-	parser = argparse.ArgumentParser()
+	try:
+		from submission.submission import *
+		parser = argparse.ArgumentParser()
 
-	#-db DATABSE -u USERNAME -p PASSWORD -size 20
-	parser.add_argument("-r", "--redis", help="Redis host URL", default=REDIS_URL)
+		#-db DATABSE -u USERNAME -p PASSWORD -size 20
+		parser.add_argument("-r", "--redis", help="Redis host URL", default=REDIS_URL)
 
-	args = parser.parse_args()
+		args = parser.parse_args()
 
-	env = PlayerHome(redis_url=args.redis)
+		env = PlayerHome(redis_url=args.redis)
 
-	obs = env.reset()
-	tic = datetime.now()
-	print("Testing your agent...")
-	for _ in range(env.num_timesteps):
-		action = predict(env)
-		env.step(action)
+		obs = env.reset()
+		tic = datetime.now()
+		print("Testing your agent...")
+		for _ in range(env.num_timesteps):
+			action = predict(env)
+			env.step(action)
 
-	asyncio.run(env.post_status("done"))
-	print(env.score())
-	toc = datetime.now()
-	print(toc-tic)
+		asyncio.run(env.post_status("done"))
+		print(env.score())
+		toc = datetime.now()
+		print(toc-tic)
+	except:
+		df = pd.DataFrame(
+			{
+				'std_demand': ['Invalid Submission'],
+				'max_demand': ['Invalid Submission'],
+				'l2_dev_avg': ['Invalid Submission'],
+			}
+		)
+		df.to_csv('./outputs/score.csv')
